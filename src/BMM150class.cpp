2593e56cb1c233ddbdd638eefb35393306d0562b
9PEO0xNd <77033891+9PEO0xNd@users.noreply.github.com>
2021-01-07 11:11:52 +0900

    Update BMM150class.cpp

--- src/BMM150class.cpp
+++ src/BMM150class.cpp
@@ -61,6 +61,8 @@ void BMM150class::bmm150_offset_save()
 {
     prefs.begin("bmm150", false);
     prefs.putBytes("offset", (uint8_t *)&mag_offset, sizeof(bmm150_mag_data));
+    // added soft-iron scale
+    prefs.putBytes("scale", (uint8_t *)&mag_scale, sizeof(bmm150_mag_data));
     prefs.end();
 }
 
@@ -69,6 +71,8 @@ void BMM150class::bmm150_offset_load()
     if (prefs.begin("bmm150", true))
     {
         prefs.getBytes("offset", (uint8_t *)&mag_offset, sizeof(bmm150_mag_data));
+        // added soft iron distortion scaling
+        prefs.getBytes("scale", (uint8_t *)&mag_scale, sizeof(bmm150_mag_data));
         prefs.end();
         Serial.printf("bmm150 load offset finish.... \r\n");
     }
@@ -77,7 +81,7 @@ void BMM150class::bmm150_offset_load()
         Serial.printf("bmm150 load offset failed.... \r\n");
     }
 }
-
+/*
 void BMM150class::setup()
 {
     M5.begin(true, false, true, false);
@@ -101,7 +105,7 @@ void BMM150class::setup()
 
     bmm150_offset_load();
 }
-
+*/
 void BMM150class::bmm150_calibrate(uint32_t calibrate_time)
 {
     uint32_t calibrate_timeout = 0;
@@ -136,54 +140,72 @@ void BMM150class::bmm150_calibrate(uint32_t calibrate_time)
     mag_offset.x = (mag_max.x + mag_min.x) / 2;
     mag_offset.y = (mag_max.y + mag_min.y) / 2;
     mag_offset.z = (mag_max.z + mag_min.z) / 2;
+    // added soft iron calibration
+    mag_chord.x = (mag_max.x - mag_min.x) / 2;
+    mag_chord.y = (mag_max.y - mag_min.y) / 2;
+    mag_chord.z = (mag_max.z - mag_min.z) / 2;
+    avg_chord = (mag_chord.x + mag_chord.y + mag_chord.z)/3;
+    mag_scale.x = avg_chord / mag_chord.x;
+    mag_scale.y = avg_chord / mag_chord.y;
+    mag_scale.z = avg_chord / mag_chord.z;
     bmm150_offset_save();
 
     Serial.printf("\n calibrate finish ... \r\n");
-    Serial.printf("mag_max.x: %.2f x_min: %.2f \t", mag_max.x, mag_min.x);
-    Serial.printf("y_max: %.2f y_min: %.2f \t", mag_max.y, mag_min.y);
-    Serial.printf("z_max: %.2f z_min: %.2f \r\n", mag_max.z, mag_min.z);
+//    Serial.printf("mag_max.x: %.2f x_min: %.2f \t", mag_max.x, mag_min.x);
+//    Serial.printf("y_max: %.2f y_min: %.2f \t", mag_max.y, mag_min.y);
+//    Serial.printf("z_max: %.2f z_min: %.2f \r\n", mag_max.z, mag_min.z);
 }
 
-int BMM150class::Init(void)
+void BMM150class::Init(void)
 {
     Wire.begin(21, 22, 400000);
-    bmm150_initialization();
-    bmm150_offset_load();
-    bmm150_read_mag_data(&dev);
-    float head_dir = atan2(dev.data.x - mag_offset.x, dev.data.y - mag_offset.y) * 180.0 / M_PI;
-    Serial.printf("Magnetometer data, heading %.2f\n", head_dir);
-    Serial.printf("MAG X : %.2f \t MAG Y : %.2f \t MAG Z : %.2f \n", dev.data.x, dev.data.y, dev.data.z);
-    Serial.printf("MID X : %.2f \t MID Y : %.2f \t MID Z : %.2f \n", mag_offset.x, mag_offset.y, mag_offset.z);
+    
     M5.Lcd.fillScreen(BLACK);
     M5.Lcd.setTextColor(WHITE);
-    M5.Lcd.setTextSize(1);
-    M5.Lcd.setCursor(0, 10);
-    M5.Lcd.printf("MAG X: %.2f", dev.data.x);
-    M5.Lcd.setCursor(0, 20);
-    M5.Lcd.printf("MAG Y: %.2f", dev.data.y);
-    M5.Lcd.setCursor(0, 30);
-    M5.Lcd.printf("MAG Z: %.2f", dev.data.z);
-    M5.Lcd.setCursor(0, 40);
-    M5.Lcd.printf("HEAD Angle: %.2f", head_dir);
-    M5.Lcd.setCursor(0, 50);
-    M5.Lcd.print("begin calibration i 5 seconds");
-    delay(5000);
-    M5.Lcd.fillScreen(BLACK);
-    M5.Lcd.setTextColor(WHITE);
-    M5.Lcd.setTextSize(1);
-    M5.Lcd.setCursor(0, 10);
-    M5.Lcd.print("Flip + rotate core calibration");
-    bmm150_calibrate(10000);
-    delay(100);
 
-    return 0;
+    if (bmm150_initialization() != BMM150_OK)
+    {
+        M5.Lcd.setCursor(0, 10);
+        M5.Lcd.print("BMM150 init failed");
+        for (;;)
+        {
+            delay(100);
+        }
+    }
+    
+    bmm150_offset_load();
+
+    Serial.printf("MID X : %.2f \t MID Y : %.2f \t MID Z : %.2f \n", mag_offset.x, mag_offset.y, mag_offset.z);
+    Serial.printf("SCALE X : %.2f \t SCALE Y : %.2f \t SCALE Z : %.2f \n", mag_scale.x, mag_scale.y, mag_scale.z);
+    M5.Lcd.setCursor(0, 10);
+    M5.Lcd.printf("offset X, Y, Z: %5.4f %5.4f %5.4f \n", mag_offset.x, mag_offset.y, mag_offset.z);
+    M5.Lcd.setCursor(0, 20);
+    M5.Lcd.printf("scale  X, Y, Z: %5.4f %5.4f %5.4f \n", mag_scale.x, mag_scale.y, mag_scale.z);
+
+    delay(3000); // see preset data 3 sec.
 }
 
 void BMM150class::getMagnetData(float *mx, float *my, float *mz)
 {
     bmm150_read_mag_data(&dev);
-    float head_dir = atan2(dev.data.x - mag_offset.x, dev.data.y - mag_offset.y) * 180.0 / M_PI;
+//    float head_dir = atan2(dev.data.x - mag_offset.x, dev.data.y - mag_offset.y) * 180.0 / M_PI;
     *mx = dev.data.x;
     *my = dev.data.y;
     *mz = dev.data.z;
+}
+
+void BMM150class::getMagnetOffset(float *mx, float *my, float *mz)
+{
+    bmm150_offset_load();
+    *mx = mag_offset.x;
+    *my = mag_offset.y;
+    *mz = mag_offset.z;
+}
+
+void BMM150class::getMagnetScale(float *mx, float *my, float *mz)
+{
+    bmm150_offset_load();
+    *mx = mag_scale.x;
+    *my = mag_scale.y;
+    *mz = mag_scale.z;
 }
